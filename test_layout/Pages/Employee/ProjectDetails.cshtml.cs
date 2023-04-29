f59d39323253a2d94383bd7342b862cd7dffe40d
salma-hatem <s-salma.hatem@zewailcity.edu.eg>
2023-04-29 13:09:47 +0200

    Employee layout

---
+++ test_layout/Pages/Employee/ProjectDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HR_DBMS.Pages.Employee
+{
+    public class ProjectDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
