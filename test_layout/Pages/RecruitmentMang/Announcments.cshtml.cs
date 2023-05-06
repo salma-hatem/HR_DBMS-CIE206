12743a4dd1d8c89cccca3eed3f00bf7dcfb2c739
mariamismael <s-mariam.ismael@zewailcity.edu.eg>
2023-05-06 16:31:05 +0300

    update announcments

--- test_layout/Pages/RecruitmentMang/Announcments.cshtml.cs
+++ test_layout/Pages/RecruitmentMang/Announcments.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace HR_DBMS.Pages
+namespace HR_DBMS.Pages.RecruitmentMang
 {
     public class AnnouncmentsModel : PageModel
     {
