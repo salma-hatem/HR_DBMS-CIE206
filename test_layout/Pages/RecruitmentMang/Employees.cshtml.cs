d168c731aeda841c4a5258efcb8b10703513deef
Noran20 <61395158+Noran20@users.noreply.github.com>
2023-05-16 21:22:01 +0300

    employee update

--- test_layout/Pages/RecruitmentMang/Employees.cshtml.cs
+++ test_layout/Pages/RecruitmentMang/Employees.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HR_DBMS.Models;
+using System.Data;
+using test_layout.Models;
+
 
 namespace HR_DBMS.Pages.RecruitmentMang
 {
     public class EmployeesModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public int ID { get; set; }
+
+        public readonly DBManager DB;
+
+        [BindProperty]
+        public DataTable Employees_table { get; set; }
+        public EmployeesModel(DBManager dB)
+        {
+            DB = dB;
+            ID = DB.GetCurrentUserID();
+        }
+
         public void OnGet()
         {
+            Employees_table = (DataTable)DB.JoinTablesWithCondition("Personal as P","Employee as E", "P.FName +' '+ P.Lname, P.id, P.Work_Email, P.Team, Person_Status",
+                "E.EmployeeID", "P.id", "P.Person_Role", "'Employee'");
+        }
+        public void OnPost()
+        {
+
         }
     }
 }
