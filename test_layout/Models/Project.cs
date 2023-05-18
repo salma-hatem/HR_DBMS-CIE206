071449b4e4ae41df0c5537a5eda1f3eb8280d08a
StudHamza <202100978@zewailcity.edu.eg>
2023-05-18 11:42:30 +0300

    All perforances and charts are left

---
+++ test_layout/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace test_layout.Models
+{
+	public class Project
+	{
+		public int ID { get; set; }
+
+        [Required,MinLength(3)]
+		public string Name { get; set; }
+        public int ManagedBy { get; set; }
+        [Required]
+        public string StartDate { get; set; }
+        [Required]
+        public string EndDate { get; set; }
+        public string Deadline { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public int ProgPercent { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Goal { get; set; }
+    }
+}
