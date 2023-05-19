c7c85ec1d52d9164dd07c594285015f3ecafb25b
Nour-Mahgoub <118842630+Nour-Mahgoub@users.noreply.github.com>
2023-05-19 17:36:46 +0300

    update training

--- test_layout/Pages/TrainingMang/Training_Update.cshtml.cs
+++ test_layout/Pages/TrainingMang/Training_Update.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using test_layout.Models;
-
+using HR_DBMS.Models;
 namespace HR_DBMS.Pages.TrainingMang
 {
     public class Training_UpdateModel : PageModel
@@ -39,22 +39,23 @@ namespace HR_DBMS.Pages.TrainingMang
         public Training_UpdateModel(DBManager db)
         {
             DB = db;
-            dt = (DataTable)DB.ReadTablesWithConditon("Training", "Training_Name,Training_Location, Training_Description", "ID", Train_id.ToString());
-
-            Train_name = (string)dt.Rows[0][0];
-            Train_location = (string)dt.Rows[0][1];
-            Train_description = (string)dt.Rows[0][2];
+            dt= new DataTable();    
 
         }
         public void OnGet(int id)
         {
             Train_id = id;
+           dt = (DataTable)DB.ReadTablesWithConditon("Training", "Training_Name,Training_Location, Training_Description", "ID", Train_id.ToString());
+
+            Train_name = (string)dt.Rows[0][0];
+            Train_location = (string)dt.Rows[0][1];
+            Train_description = (string)dt.Rows[0][2];
             //dt = (DataTable)DB.JoinTablesWithCondition("Training as T","Training_Date as TD", 
             //    "Training_Name ,Training_Location,Training_Description,Training_Time,Training_StartDate ,Training_EndDate",
             //    "TD.ID", "T.ID","T.ID",id.ToString());
-            
-           // Train_start_date = (DateTime)dt.Rows[0][3];
-           // Train_end_date = (DateTime)dt.Rows[0][4];
+
+            // Train_start_date = (DateTime)dt.Rows[0][3];
+            // Train_end_date = (DateTime)dt.Rows[0][4];
 
         }
     }
