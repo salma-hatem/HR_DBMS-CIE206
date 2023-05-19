cf653585ddccc9a4f48e38cf806c767f177de882
Noran20 <61395158+Noran20@users.noreply.github.com>
2023-05-19 17:57:58 +0300

    add and abit of edit employee

--- test_layout/Models/DBManager.cs
+++ test_layout/Models/DBManager.cs
@@ -654,32 +654,113 @@ namespace test_layout.Models
                 con.Close();
             }
         }
-        public void AddRecordEmployee(string Name, int id, string email, string status, string team, string Add, int salary, int ssn, string role, string contact, int age, string sex)
+        public int getEmployeeID()
         {
-            //    string query = "INSERT INTO Personal VALUES (@ID,@Name,@Email,@UserName,@Password,@Type)";
-            //    SqlCommand cmd = new SqlCommand(query, con);
-            //    cmd.Parameters.AddWithValue("@ID", id);
-            //    cmd.Parameters.AddWithValue("@Name", Name);
-            //    cmd.Parameters.AddWithValue("@Email", email);
-            //    cmd.Parameters.AddWithValue("@UserName", username);
-            //    cmd.Parameters.AddWithValue("@Password", password);
-            //    cmd.Parameters.AddWithValue("@Type", type);
-            //    try
-            //    {
-            //        con.Open();
-            //        cmd.ExecuteNonQuery();
-            //    }
-            //    catch (SqlException ex)
-            //    {
-            //        Console.WriteLine(ex.Message);
-            //    }
-            //    finally
-            //    {
-            //        con.Close();
-            //    }
-            //}
+            string query = "select MAX(id) from Personal";
+            SqlCommand cmd = new SqlCommand(query, con);
+            int ID = 0;
+            try
+            {
+                con.Open();
+                ID = (int)cmd.ExecuteScalar();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return (ID + 1);
+        }
+        public void AddRecordPerson(string FName, string LName, int ID, string email,int pass, string status, string team, string Add, int salary, int ssn, string role, string contact, int age, string sex, string img, int holidays)
+        {
+            
+            string query = "INSERT INTO Personal VALUES (@id, @SSN,@Team,@Sex,@FName, @LName,@Age, @Email, @Work_Email,@Person_Password, @Person_Address,@Person_Status,@Salary, @Person_Role,@Contact_Num,@Person_IMG, @Holidays )";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@id", ID);
+            cmd.Parameters.AddWithValue("@SSN", ssn);
+            cmd.Parameters.AddWithValue("@Team", team);
+            cmd.Parameters.AddWithValue("@Sex",sex);
+            cmd.Parameters.AddWithValue("@FName", FName);
+            cmd.Parameters.AddWithValue("@LName", LName);
+            cmd.Parameters.AddWithValue("@Age", age);
+            cmd.Parameters.AddWithValue("@Email", email);
+            cmd.Parameters.AddWithValue("@Work_Email", email);
+            cmd.Parameters.AddWithValue("@Person_Status", status);
+            cmd.Parameters.AddWithValue("@Person_Address", Add);
+            cmd.Parameters.AddWithValue("@Person_Password", pass);
+            cmd.Parameters.AddWithValue("@Salary", salary);
+            cmd.Parameters.AddWithValue("@Person_Role", role);
+            cmd.Parameters.AddWithValue("@Contact_Num", ID);
+            cmd.Parameters.AddWithValue("@Person_IMG", img);
+            cmd.Parameters.AddWithValue("@Holidays", holidays);
+
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
 
         }
-    }
 
+        public void AddRecordEmployee(int ID, int RMID, int PMID)
+        {
+            
+            string query = "INSERT INTO Employee VALUES (@EmployeeID, @PMID ,@RMID)";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@EmployeeID", ID);
+            cmd.Parameters.AddWithValue("@RMID", RMID);
+            cmd.Parameters.AddWithValue("@PMID", PMID);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+        public string getItemEmployee(int id, string itemname)
+        {
+            string var ="";
+            string query = "select "+ itemname+ "from personal where id=" +id ;
+            SqlCommand cmd= new SqlCommand(query, con);
+            try
+            {
+               
+                con.Open();
+                var= (string)cmd.ExecuteScalar();
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return var;
+        }
+    }
 }
